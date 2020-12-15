d60369caffc7144a3a8b0a3218f9b89fab9dd5b7
Michael <michaelpadillaheredia@hotmail.com>
2020-12-15 16:23:35 -0500

    fillingBoard

--- js/boardGenerationFunctions.js
+++ js/boardGenerationFunctions.js
@@ -18,3 +18,42 @@ export const createBoard = (regions, board) => {
     createTiles(9, region)
     createBoard(regions - 1, board)
 }
+
+export const addRegionIds = (keyword, initial) => {
+    const regions = Array.from(document.getElementsByClassName('region'))
+
+    regions.forEach(region => region.id = `${keyword} ${initial ++}`)
+}
+
+const arrayFromTo = (from, to, array) => {
+    if (from > to) return array
+    array.push(from)
+
+    return arrayFromTo(from + 1, to, array)
+}
+
+export const fillBoard = board => {
+    let bidArray = new Array(9)
+    bidArray.fill(0)
+
+    bidArray.forEach((n, i) => {
+	n = arrayFromTo(1, 9, [])
+	let reg = document.getElementById(`region ${i}`)
+
+	const tiles = reg.getElementsByClassName('tile')
+
+	n.forEach((nn, ii) => {
+	    let tile = tiles[ii]
+	    let txt = document.createTextNode(nn)
+	    tile.appendChild(txt)
+
+	    tile.id = `tile ${i} ${ii}`
+	    tile.addEventListener('click', evt => {console.log('heyy im ', evt.path[0].id)})
+	})
+
+	bidArray[i] = (n)
+    })
+
+    console.log(bidArray)
+}
+
--- js/loader.js
+++ js/loader.js
@@ -1,8 +1,11 @@
-import { createBoard } from './boardGenerationFunctions.js'
+import { createBoard, addRegionIds, fillBoard } from './boardGenerationFunctions.js'
 
 window.addEventListener('load', () => {
     const board = document.getElementById('board')
+
     createBoard(9, board)
+    addRegionIds('region', 0)
+    fillBoard(board)
 })
 
 
